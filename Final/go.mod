42b10dd4e1e97d1df7d2ded8df6f4fa370d905b0
Ball1010 <diegoapelayo18@gmail.com>
2023-12-26 15:47:36 -0800

    Setting up Hello() call from beginning.go in main.go

---
+++ Final/go.mod
@@ -0,0 +1,5 @@
+module github.com/Ball1010/95Die/Final
+
+go 1.21.5
+
+require github.com/Ball1010/95Die/Beginning v0.0.0-20231226230404-39b55d96a7f0
---
+++ Final/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Ball1010/95Die/Beginning"
+	"log"
+)
+
+func main(){
+	log.SetPrefix("beginning: ")
+	log.SetFlags(0)
+
+	msgf, err:= beginning.Hello("")
+	if err !=nil {
+		log.Fatal(err)
+	}
+	fmt.Println(msgf)
+}
